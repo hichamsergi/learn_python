ca0bd8a1e337f6237efd09960d9ddf3212f4a7b0
isx39444871 <hichamsergi@gmail.com>
2025-02-26 18:53:54 +0100

    new start

--- Hacker_Rank/chapter1/01_List_comprehension.py
+++ Hacker_Rank/chapter1/01_List_comprehension.py
@@ -2,9 +2,8 @@ if __name__ == '__main__':
     x = int(input())
     y = int(input())
     z = int(input())
-    
     n = int(input())
     
-    lista = [[i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if (i + j + k) != n]
+    cuboid = [[i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k != n]
     
-    print(lista)
+    print(cuboid)
