7ca84a5181a44fd697797af314e589976727872f
isx39444871 <hichamsergi@gmail.com>
2025-02-26 19:30:20 +0100

    new start

--- Hacker_Rank/chapter1/02_RunnerUp_Score.py
+++ Hacker_Rank/chapter1/02_RunnerUp_Score.py
@@ -1,25 +1,16 @@
 if __name__ == '__main__':
-    
     n = int(input())
-    arr = input().split()
-        
-    arr = [int(i) for i in arr]
-    up=[-100,-99]
+    arr = list(map(int, input().split()))
 
+    second=[-100,-100]
 
+    for num in range(len(arr)):
 
+        if arr[num] < second[1] and arr[num] > second[0]:
+            second[0] = arr[num]
 
-    for i in range(len(arr)):
-        
-        if arr[i] not in up:
-            
-            if arr[i] > up[1]:
-                
-                del up[0]
-                up.append(arr[i])
-            
-            elif arr[i] < up[1] and arr[i] > up[0]:
-                
-                up[0] = arr[i]
-        
-    print(up[0])
+        elif arr[num] > second[1]:
+            second[0] = second[1]
+            second[1] = arr[num]
+
+    print(second[0])
