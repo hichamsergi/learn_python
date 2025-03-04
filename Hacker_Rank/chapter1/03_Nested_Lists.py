2491615cad7ba0fc13b8699f9928e79fb644e3fd
hichamsergi <hichamsergi@gmail.com>
2025-03-04 17:00:46 +0100

    new start

--- Hacker_Rank/chapter1/03_Nested_Lists.py
+++ Hacker_Rank/chapter1/03_Nested_Lists.py
@@ -9,18 +9,15 @@ if __name__ == '__main__':
         name = input()
         score = float(input())
 
-        #Si la puntuación es mas baja que la mas baja:
         if score < max_low:
             sec_low = max_low
             max_low = score
-        #Si la puntuación es la segunda mas baja:
+
         elif score < sec_low and score != max_low:
             sec_low = score
         
-        #Añadimos el record a records
         records.append([name,score])
 
-    #Localizar y mostrar los nombres que coinciden con la segunda puntuación mas baja:
 
     names=[rec[0] for rec in records if rec[1] == sec_low]
     names.sort()
