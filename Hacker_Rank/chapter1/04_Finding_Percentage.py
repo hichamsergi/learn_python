71b15256fa6a9f06a13bdc100adc5e5d948d982b
hichamsergi <hichamsergi@gmail.com>
2025-03-05 10:38:26 +0100

    new start

--- Hacker_Rank/chapter1/04_Finding_Percentage.py
+++ Hacker_Rank/chapter1/04_Finding_Percentage.py
@@ -1,15 +1,15 @@
 if __name__ == '__main__':
-    
     n = int(input())
-    
-    student_marks = {}
-    
+    s_marks = {}
+
     for _ in range(n):
         name, *line = input().split()
         scores = list(map(float, line))
-        student_marks[name] = scores
+        s_marks[name] = scores
     
-    query_name = input()
-    avg_percentage = sum(student_marks[query_name]) / len(student_marks[query_name])
+    q_name = input()
+    
+    total = sum(s_marks.get(q_name)) / len(s_marks.get(q_name))
 
-    print('%.2f' % avg_percentage)
+    print('%.2f' % total)
+    
